77ade74629a438faad63b01950f04f2ba0153c19
aliftareq <aliftareq@gmail.com>
2022-11-24 19:49:05 +0600

    authentication complete

--- src/Pages/AuthenticationPages/LoginPage/Login.js
+++ src/Pages/AuthenticationPages/LoginPage/Login.js
@@ -1,10 +1,110 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { AuthContext } from '../../../Contexts/AuthProvider';
 
 const Login = () => {
+    //context value 
+    const { signIn, LoginWithGoogle } = useContext(AuthContext)
+    //states
+    const [loginError, setloginError] = useState()
+    const [loginUserEmail, setLoginUserEmail] = useState('')
+
+    //token hook
+    // const [token] = useToken(loginUserEmail)
+
+    //navigation
+    // const navigate = useNavigate()
+    // const location = useLocation()
+    // const from = location?.state?.from?.pathname || '/'
+
+    //navigate
+    // if (token) {
+    //     navigate(from, { replace: true })
+    // }
+
+    //react form hook
+    const { register, formState: { errors }, handleSubmit } = useForm()
+
+    //handlers
+    const handleLogin = (data) => {
+
+        console.log(data);
+        signIn(data.email, data.password)
+            .then(result => {
+                const user = result.user
+                console.log(user);
+                setLoginUserEmail(data.email)
+                setloginError('')
+                toast.success('Successfully logged in')
+
+            })
+            .catch(err => {
+                console.error(err)
+                setloginError(err.message)
+            })
+    }
+    //handler for social login
+    const handleGoogleSignIn = () => {
+        LoginWithGoogle()
+            .then(result => {
+                console.log(result.user);
+                toast.success('successfully sign-In with google')
+                // navigate(from, { replace: true })
+            })
+            .catch(err => {
+                console.log(err);
+                toast.error(err.message)
+            })
+    }
     return (
-        <div>
-            <h1>this is Login Page</h1>
-        </div>
+        <section className='h-[500px] lg:h-[600px] flex justify-center items-center'>
+            <div className='w-64 lg:w-96'>
+                <h1 className='text-4xl text-center'>Login</h1>
+                <form onSubmit={handleSubmit(handleLogin)}>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">E-mail</span>
+                        </label>
+                        <input type="email"
+                            {...register("email", { required: "Email Address is required" })}
+                            placeholder="example@gmail.com"
+                            className="input input-bordered w-full"
+                        />
+                        {errors.email && <p className='text-red-400' role="alert">{errors.email?.message}</p>}
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password"
+                            placeholder="*******"
+                            className="input input-bordered w-full"
+                            {...register("password", {
+                                required: 'Password is required',
+                                minLength: { value: 6, message: 'Password Must be 6 Character or longer' },
+
+                            })} />
+                        {errors.password && <p className='text-red-400' role="alert">{errors.password?.message}</p>}
+                        <label className="label">
+                            <span className="label-text">Forget Password</span>
+                        </label>
+                    </div>
+                    <input className='btn btn-accent w-full' value='Login' type="submit" />
+                    <div>
+                        {loginError && <p className='text-red-400'>{loginError}</p>}
+                    </div>
+                </form>
+                <p className='text-center mt-3'>New to Doctors Portal?
+                    <span className='text-secondary ml-2 underline block lg:inline'>
+                        <Link to='/register'>Create new account</Link>
+                    </span>
+                </p>
+                <div className="divider">OR</div>
+                <button onClick={handleGoogleSignIn} className='btn btn-outline  w-full'>CONTINUE WITH GOOGLE</button>
+            </div>
+        </section>
     );
 };
 
--- src/Pages/Shared/NavBar/NavBar.js
+++ src/Pages/Shared/NavBar/NavBar.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { AuthContext } from '../../../Contexts/AuthProvider';
 
 const NavBar = () => {
+    //context values
+    const { user, logOut } = useContext(AuthContext)
+    //handlers 
+    const handleLogOut = () => {
+        logOut()
+            .then(() => {
+                toast.info('successfully logged out')
+            })
+            .catch(err => {
+                console.error(err)
+                toast.error('something went wrong')
+            })
+    }
     return (
         <section>
             <div className="navbar bg-base-100">
@@ -10,90 +26,120 @@ const NavBar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Home</a></li>
+                            <li><Link to='/'>Home</Link></li>
                             <li tabIndex={0}>
-                                <a className="justify-between">
+                                <Link className="justify-between">
                                     Categories
                                     <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                                </a>
+                                </Link>
                                 <ul className="p-2">
-                                    <li><a>pickup/van </a></li>
-                                    <li><a>trucks </a></li>
-                                    <li><a>trailor </a></li>
+                                    <li><Link>pickup/van </Link></li>
+                                    <li><Link>trucks </Link></li>
+                                    <li><Link>trailor </Link></li>
                                 </ul>
                             </li>
-                            <li><a>Blog</a></li>
-                            <li><a>DashBoard</a></li>
-                            <li><a>Login</a></li>
-                            <li tabIndex={1}>
-                                <a className="justify-between">
-                                    <div className="w-10 rounded-full">
-                                        <img className='rounded-lg' src="https://placeimg.com/80/80/people" />
-                                    </div>
-                                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                                </a>
-                                <ul className="p-2">
-                                    <li>
-                                        <a className="justify-between">
-                                            Profile
-                                        </a>
+                            <li><Link>Blog</Link></li>
+                            {
+                                user && user?.uid
+                                    ?
+                                    <>
+                                        <li><Link>DashBoard</Link></li>
+                                        <li tabIndex={1}>
+                                            <Link className="justify-between">
+                                                <div className="w-10 rounded-full">
+                                                    <img title={user?.displayName} src={user?.photoURL} alt='img-1' />
+                                                </div>
+                                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
+                                            </Link>
+                                            <ul className="p-2">
+                                                <li>
+                                                    <Link className="justify-between">
+                                                        Profile
+                                                    </Link>
+                                                </li>
+                                                <li><Link>My Products</Link></li>
+                                            </ul>
+                                        </li>
+                                    </>
+                                    :
+                                    <li><Link to='/login'>Login</Link></li>
+                            }
+                            {
+                                user && user?.uid
+                                    ?
+                                    <li onClick={handleLogOut}>
+                                        <p className="btn">Logout</p>
+
                                     </li>
-                                    <li><a>Logout</a></li>
-                                </ul>
-                            </li>
-                            <li tabIndex={2}>
-                                <a className="btn">Register</a>
-                                <ul className="p-2">
-                                    <li><a>Join as a Seller</a></li>
-                                    <li><a>Join as a Buyer</a></li>
-                                </ul>
-                            </li>
+                                    :
+                                    <Link to='/register'>
+                                        <li>
+                                            <p className="btn">Register</p>
+                                        </li>
+                                    </Link>
+
+                            }
+
+
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <Link className="btn btn-ghost normal-case text-xl">daisyUI</Link>
                 </div>
                 <div className="navbar-end hidden lg:flex">
                     <ul className="menu menu-horizontal p-0">
-                        <li><a>Home</a></li>
+                        <li><Link to='/'>Home</Link></li>
                         <li tabIndex={0}>
-                            <a>
+                            <Link>
                                 Categories
                                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                            </a>
+                            </Link>
                             <ul className="p-2">
-                                <li><a>Pick-Up/Van</a></li>
-                                <li><a>Trucks</a></li>
-                                <li><a>Trailor</a></li>
+                                <li><Link>Pick-Up/Van</Link></li>
+                                <li><Link>Trucks</Link></li>
+                                <li><Link>Trailor</Link></li>
                             </ul>
                         </li>
-                        <li><a>Blog</a></li>
-                        <li><a>Dashboard</a></li>
-                        <li><a>Login</a></li>
+                        <li><Link>Blog</Link></li>
+                        {
+                            user && user?.uid
+                                ? <li><Link>Dashboard</Link></li>
+                                :
+                                <li><Link to='/login'>Login</Link></li>
+                        }
+
                     </ul>
-                    <div className="dropdown dropdown-end">
-                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img src="https://placeimg.com/80/80/people" />
+                    {
+                        user && user?.uid
+                            ?
+                            <div className="dropdown dropdown-end">
+                                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                    <div className="w-10 rounded-full">
+                                        <img title={user?.displayName} src={user?.photoURL} alt='img-1' />
+                                    </div>
+                                </label>
+                                <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                                    <li>
+                                        <Link>Profile</Link>
+                                    </li>
+                                    <li><Link>My Products</Link></li>
+                                </ul>
                             </div>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li>
-                                <a className="justify-between">
-                                    Profile
-                                </a>
-                            </li>
-                            <li><a>Logout</a></li>
-                        </ul>
-                    </div>
-                    <div className="dropdown dropdown-end">
-                        <label tabIndex={0} className="btn ">
-                            <a className="">Register</a>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Join as a buyer</a></li>
-                            <li><a>Join as a seller</a></li>
-                        </ul>
-                    </div>
+                            : <></>
+
+                    }
+                    {
+                        user && user?.uid
+                            ?
+                            <div onClick={handleLogOut} className="btn">
+                                <p>Logout</p>
+                            </div>
+                            :
+                            <Link to='/register'>
+                                <div className="btn">Register</div>
+                            </Link>
+                    }
+
+
                 </div>
             </div>
         </section>
