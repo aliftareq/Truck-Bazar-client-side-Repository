b4f059581eae0fdf7f6f4204ed3b3772564d6c4a
aliftareq <aliftareq@gmail.com>
2022-11-25 10:38:09 +0600

    navbar modified

--- src/Pages/Shared/NavBar/NavBar.js
+++ src/Pages/Shared/NavBar/NavBar.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { AuthContext } from '../../../Contexts/AuthProvider';
+import logo from '../../../Assets/images/tb-logo.jpg'
 
 const NavBar = () => {
     //context values
@@ -23,11 +24,11 @@ const NavBar = () => {
     }
     return (
         <section>
-            <div className="navbar bg-gray-900">
+            <div className="navbar bg-gray-900 lg:px-8">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                             <li><Link to='/'>Home</Link></li>
@@ -36,10 +37,10 @@ const NavBar = () => {
                                     Categories
                                     <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
                                 </Link>
-                                <ul className="p-2">
-                                    <li><Link>pickup/van </Link></li>
-                                    <li><Link>trucks </Link></li>
-                                    <li><Link>trailor </Link></li>
+                                <ul className="p-2 bg-indigo-50 rounded">
+                                    <li className='text-black'><Link>pickup/van </Link></li>
+                                    <li className='text-black'><Link>trucks </Link></li>
+                                    <li className='text-black'><Link>trailor </Link></li>
                                 </ul>
                             </li>
                             <li><Link>Blog</Link></li>
@@ -51,17 +52,17 @@ const NavBar = () => {
                                         <li tabIndex={1}>
                                             <Link className="justify-between">
                                                 <div className="w-10 rounded-full">
-                                                    <img title={user?.displayName} src={user?.photoURL} alt='img-1' />
+                                                    <img className='rounded-2xl' title={user?.displayName} src={user?.photoURL} alt='img-1' />
                                                 </div>
                                                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
                                             </Link>
-                                            <ul className="p-2">
-                                                <li>
+                                            <ul className="p-2 bg-indigo-50 rounded">
+                                                <li className='text-black'>
                                                     <Link className="justify-between">
                                                         Profile
                                                     </Link>
                                                 </li>
-                                                <li><Link>My Products</Link></li>
+                                                <li className='text-black'><Link>My Products</Link></li>
                                             </ul>
                                         </li>
                                     </>
@@ -72,13 +73,13 @@ const NavBar = () => {
                                 user && user?.uid
                                     ?
                                     <li onClick={handleLogOut}>
-                                        <p className="btn">Logout</p>
+                                        <p className="btn bg-indigo-500 hover:bg-indigo-700 border-none">Logout</p>
 
                                     </li>
                                     :
                                     <Link to='/register'>
                                         <li>
-                                            <p className="btn">Register</p>
+                                            <p className="btn bg-indigo-500 hover:bg-indigo-700 border-none">Register</p>
                                         </li>
                                     </Link>
 
@@ -87,7 +88,14 @@ const NavBar = () => {
 
                         </ul>
                     </div>
-                    <Link className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+                    <div className='flex items-center'>
+                        <Link to='/' className="text-white text-xl font-bold">
+                            <img className='w-16 rounded-md' src={logo} alt="" />
+                        </Link>
+                        <Link to='/' >
+                            <p className="text-white text-sm lg:text-xl font-bold ml-3">Truck Bazar</p>
+                        </Link>
+                    </div>
                 </div>
                 <div className="navbar-end hidden lg:flex text-white">
                     <ul className="menu menu-horizontal p-0">
@@ -97,10 +105,10 @@ const NavBar = () => {
                                 Categories
                                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
                             </Link>
-                            <ul className="p-2">
-                                <li><Link>Pick-Up/Van</Link></li>
-                                <li><Link>Trucks</Link></li>
-                                <li><Link>Trailor</Link></li>
+                            <ul className="p-2 bg-indigo-50 rounded">
+                                <li className='text-black'><Link>Pick-Up/Van</Link></li>
+                                <li className='text-black'><Link>Trucks</Link></li>
+                                <li className='text-black'><Link>Trailor</Link></li>
                             </ul>
                         </li>
                         <li><Link>Blog</Link></li>
@@ -139,7 +147,7 @@ const NavBar = () => {
                             </div>
                             :
                             <Link to='/register'>
-                                <div className="btn">Register</div>
+                                <div className="btn bg-indigo-500 hover:bg-indigo-700">Register</div>
                             </Link>
                     }
 
